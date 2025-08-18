2f177e6a05d0f8f4904a21bc28874a66c7d8ad5c
Demedah <145370036+Demedah@users.noreply.github.com>
2025-08-19 03:01:48 +0700

    Update uji_citra.py

--- uji_citra.py
+++ uji_citra.py
@@ -77,7 +77,7 @@ X = np.array(X)
 y = np.array(y)
 
 # Split & Train
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 acc = accuracy_score(y_test, model.predict(X_test))
