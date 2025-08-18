894f65b6f80d44eba0b9b6700fc0061b3f6d40a3
Demedah <145370036+Demedah@users.noreply.github.com>
2025-08-19 03:56:13 +0700

    Update uji_citra.py

--- uji_citra.py
+++ uji_citra.py
@@ -55,7 +55,7 @@ def ekstrak_fitur_gambar(img_input):
 # ============ 2) Load Dataset dari GitHub ============
 
 image_dir = "Extraksi"
-df = pd.read_csv(databaseJBC.csv)
+df = pd.read_csv("databaseJBC.csv")
 
 
 
